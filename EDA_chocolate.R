d39078f9bf805e3a105042b9eca9c938a2e74196
xnuray98s <66944976+xnuray98s@users.noreply.github.com>
2022-06-22 10:07:49 +0300

    EDA 2

--- EDA_chocolate.R
+++ EDA_chocolate.R
@@ -17,24 +17,30 @@ summary(chocolate)
 
 get_dupes(chocolate)# No duplicates
 
-# Rename Nulls
+# Check for nulls
 lapply(chocolate,function(x) { length(which(is.na(x)))}) 
 
-chocolate$cocoa_percent <- (as.double(sub("%", "", chocolate$cocoa_percent)))/100 # Percentage chr > dbl
+chocolate$cocoa_percent <- as.double(sub("%", "", chocolate$cocoa_percent))# Percentage chr > dbl
 
-chocolate$ingredients = substr(chocolate$ingredients, 4, nchar(chocolate$ingredients))
-chocolate$ingredients
+chocolate$ingredients = substr(chocolate$ingredients, 3, nchar(chocolate$ingredients))
+chocolate$ingredients = trimws(chocolate$ingredients, which = "both")
+chocolate %>% 
+  separate(ingredients, c("str1", "str2", "str3", "str4", "str5", "str6"), ",") -> chocolate
 
 chocolate %>% 
-  separate(ingredients, c("B", "S", "S*", "C", "V", "L", "Sa"), ",") -> chocolate
-
-chocolate %>% 
-  mutate(beans = as.integer(!is.na(B)), sugar = as.integer(!is.na(S)), 
-         sweetener = as.integer(!is.na(`S*`)), cocoa_butter= as.integer(!is.na(C)), 
-         vanilla = as.integer(!is.na(V)),  lecithin = as.integer(!is.na(L)),  salt = as.integer(!is.na(Sa))) -> chocolate
+  mutate(beans = as.integer(str1 == "B"), sugar = as.integer(str2 == "S"), 
+         sweetener = as.integer(str2 == "S*" | str3 == "S*"), cocoa_butter= as.integer(str2 == "C" | str3 == "C" | str4 == "C"), 
+         vanilla = as.integer(str2 == "V" | str3 == "V" | str4 == "V" | str5 == "V"),  lecithin = as.integer(str2 == "L" | str3 == "L" | str4 == "L" | str5 == "L" | str6 == "L"),  salt = as.integer(str2 == "Sa" | str3 == "Sa" | str4 == "Sa" | str5 == "Sa" | str6 == "Sa")) -> chocolate
   
 chocolate %>% 
-  select(-B, -S, -`S*`, -C, -V, -L, -Sa) -> chocolate
+  select(-str1, -str2, -str3, -str4, -str5, -str6) -> chocolate
+
+chocolate %>% 
+  pivot_longer(cols = c(beans, sugar, sweetener, 
+                        cocoa_butter, vanilla, lecithin, salt), 
+               names_to = "ingredient", values_to = "is_ingredient", values_drop_na = TRUE) -> chocolate 
+
+
 
 ## EDA
 
@@ -72,12 +78,12 @@ chocolate %>%
 
 # Pie chart
 
-# chocolate %>% 
-#   group_by(country_of_bean_origin) %>% # Group by origin
-#   filter(n() > 50) %>% # Limit to those with at least 50 observations
-#   mutate(count = n()) %>% # Add the count column
-#   ggplot(aes(x = reorder(country_of_bean_origin, count))) + 
-#   geom_bar() + 
+chocolate %>%
+  group_by(country_of_bean_origin) %>% # Group by origin
+  filter(n() > 50) %>% # Limit to those with at least 50 observations
+  mutate(count = n()) %>% # Add the count column
+  ggplot(aes(x = reorder(country_of_bean_origin, count))) +
+  geom_bar() 
 
 
 # Frequency of company location's Hanadi
@@ -96,12 +102,40 @@ chocolate %>%
       # Country of origin Sara
       # Cocoa Percent Nourah
       # Ingredients Nourah
-      
+
+# Cocoa Percent vs Rating Nourah
+ggplot(chocolate, aes(rating, cocoa_percent)) +
+  geom_jitter(width = 0.25, alpha = 0.5) +
+  geom_smooth(method = "lm", se = FALSE)
+
+# Ingredients vs Rating  Nourah
+# this is the distribution but it doesn't say much
+chocolate %>% 
+  group_by(ingredient) %>% 
+  filter(is_ingredient == 1) %>% 
+  ggplot(aes(x = rating, fill = ingredient)) +
+  geom_bar()
+
+# Outliers
+chocolate %>% 
+  group_by(ingredient) %>% 
+  filter(is_ingredient == 1) %>% 
+  ggplot(aes(x = ingredient, y = rating, fill = ingredient)) +
+  geom_boxplot()
+
+chocolate %>% 
+  group_by(ingredient) %>% 
+  filter(is_ingredient == 1) %>% 
+  ggplot(aes(x=rating, group=ingredient, fill=ingredient)) +
+  geom_density() +
+  facet_wrap(~ingredient) 
+
 # How did the taste of customers change overtime? (ingredients per years) Refal
 
 # How did the cocoa persent change overtime? Hanadi
 
 # Check for outliers... Refal
+
 
 # ------------------------
 
